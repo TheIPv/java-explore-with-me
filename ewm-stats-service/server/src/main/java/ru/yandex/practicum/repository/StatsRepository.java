107fa9af56da340601eabf27fc0a10a6b7b36231
TheIPv <anymaxi@yandex.ru>
2023-07-04 18:17:29 +0300

    fix: db

--- ewm-stats-service/server/src/main/java/ru/yandex/practicum/repository/StatsRepository.java
+++ ewm-stats-service/server/src/main/java/ru/yandex/practicum/repository/StatsRepository.java
@@ -11,17 +11,18 @@ import java.util.List;
 
 public interface StatsRepository extends JpaRepository<EndpointHit, Integer> {
 
-    @Query(" SELECT eh FROM hits eh " +
-            "WHERE eh.timestamp BETWEEN :start AND :end " +
-            "AND (eh.uri IN :uris OR :uris is NULL) " +
-            "GROUP BY eh.app, eh.uri " +
-            "ORDER BY COUNT(DISTINCT eh.ip) DESC ")
+    @Query(" SELECT h " +
+            "FROM hits h " +
+            "WHERE h.timestamp BETWEEN :start AND :end " +
+            "AND (h.uri IN :uris OR :uris is NULL) " +
+            "GROUP BY h.id, h.app, h.uri " +
+            "ORDER BY COUNT(DISTINCT h.ip) DESC ")
     List<ViewStats> getUniqueViewStats(LocalDateTime start, LocalDateTime end, List<String> uris, PageRequest pageable);
 
-    @Query(" SELECT eh FROM hits eh " +
-            "WHERE eh.timestamp BETWEEN :start AND :end " +
-            "AND (eh.uri IN :uris OR :uris is NULL) " +
-            "GROUP BY eh.app, eh.uri " +
-            "ORDER BY COUNT(eh.ip) DESC ")
+    @Query(" SELECT h FROM hits h " +
+            "WHERE h.timestamp BETWEEN :start AND :end " +
+            "AND (h.uri IN :uris OR :uris is NULL) " +
+            "GROUP BY h.id, h.app, h.uri " +
+            "ORDER BY COUNT(h.ip) DESC ")
     List<ViewStats> getViewStats(LocalDateTime start, LocalDateTime end, List<String> uris, PageRequest pageable);
 }
