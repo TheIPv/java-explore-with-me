d34636fae06a83200c75eff23d7e652b44b60db2
TheIPv <anymaxi@yandex.ru>
2023-07-19 17:26:09 +0300

    style: codestyle

--- ewm-stats-service/client/src/main/java/ru/yandex/practicum/StatsClient.java
+++ ewm-stats-service/client/src/main/java/ru/yandex/practicum/StatsClient.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
